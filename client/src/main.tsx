5797d389a5cfe51bb23c295ec624b44a0e222078
l-merta <lukas.merta26@gmail.com>
2024-11-06 22:34:24 +0100

    Styles folder + scss

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import './main.css'
+import './styles/index.css'
+import './styles/main.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
