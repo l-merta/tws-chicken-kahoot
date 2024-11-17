4e0d253a2c2d458047065bc8db116bd9e14ee922
l-merta <lukas.merta26@gmail.com>
2024-11-17 15:31:22 +0100

    Main page text

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -27,6 +27,28 @@ const App: React.FC = () => (
         <img src="images/main_bg.png" alt="" />
       </div>
     </main>
+    <div id="text"></div>
+    <section>
+      <img src="images/s1.png" alt="" />
+      <div className="text">
+        <h2>Domestikace a historie</h2>
+        <p>Slepice jsou jedním z nejstarších domestikovaných zvířat, jejichž původ sahá do jihovýchodní Asie, kde byly původně chovány pro kohoutí zápasy a vejce. Archeologické nálezy ukazují, že se slepice rozšířily do Evropy a dalších částí světa díky obchodním cestám. Dnes jsou základem zemědělství a jedním z nejčastěji chovaných zvířat na světě, poskytující lidem nejen vejce a maso, ale také významný zdroj hnojiva a pestřejší zemědělský ekosystém.</p>
+      </div>
+    </section>
+    <section>
+      <div className="text">
+        <h2>Biologie a chování</h2>
+        <p>Slepice jsou známé svou společenskou povahou a hierarchickým uspořádáním nazývaným „slepice pánve“ nebo „klovací pořádek“. Tento systém určuje, která slepice je ve skupině dominantní a má přístup k nejlepším zdrojům potravy. Slepice mají také překvapivě dobré kognitivní schopnosti a jsou schopné rozpoznávat až 100 jednotlivých členů své skupiny, stejně jako různé tváře lidí.</p>
+      </div>
+      <img src="images/s2.png" alt="" />
+    </section>
+    <section>
+      <img src="images/s3.png" alt="" />
+      <div className="text">
+        <h2>Význam pro životní prostředí</h2>
+        <p>Chov slepic na venkově nebo v menších domácích podmínkách přináší výhody pro ekologii i udržitelnost. Slepice jsou přirozenými recyklátory, protože konzumují zbytky z domácnosti a přeměňují je na vejce bohatá na živiny. Jejich přítomnost v zahradách může přispět ke snížení počtu škůdců, zatímco jejich trus je cenným hnojivem pro zlepšení kvality půdy. Tento cyklus vytváří udržitelnější a méně odpadní přístup k životu.</p>
+      </div>
+    </section>
   </>
 );
 
